5605b8a3cb4f77e46dd54b8f2469a36ebbfb2fb0
rmuuu <rgdcmontoya@gmail.com>
2024-11-29 16:04:14 +0800

    Fixed profile stuff

--- app/src/main/java/com/example/mobilecomputing/Activity/ProfileActivity.java
+++ app/src/main/java/com/example/mobilecomputing/Activity/ProfileActivity.java
@@ -1,17 +1,11 @@
 package com.example.mobilecomputing.Activity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Button;
-import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -19,17 +13,13 @@ import androidx.appcompat.widget.Toolbar;
 import com.bumptech.glide.Glide;
 import com.example.mobilecomputing.Dashboard;
 import com.example.mobilecomputing.R;
-import com.example.mobilecomputing.UpdateProfileHelper;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.StorageReference;
-import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
 
